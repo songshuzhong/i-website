bbf18a54cb13778f8520201d072e3a58c0322441
songshuzhong <18346667355@163.com>
2025-05-17 16:34:30 +0800

    update

--- src/component/ToMobile.vue
+++ src/component/ToMobile.vue
@@ -9,18 +9,16 @@ export default defineComponent({
   setup() {
     const {proxy} = getCurrentInstance();
     const onClick = () => {
-      const isPro = process.env.NODE_ENV === 'production';
       let pageId = '8f287653-1059-414b-9485-dffbfe91d2b0';
-      if (window.IRenderer.pageInfo && window.IRenderer.pageInfo.pageId) {
-        pageId = window.IRenderer.pageInfo.pageId;
+      if (proxy.$.appContext.$IRenderer.pageInfo && proxy.$.appContext.$IRenderer.pageInfo.pageId) {
+        pageId = proxy.$.appContext.$IRenderer.pageInfo.pageId;
       }
       proxy.$dispatchAction(
         proxy,
-        {},
         {
           renderer: 'action',
           actionType: 'url',
-          url: isPro? 'https://songshuzhong.github.io/i-website/dist/mobile.html?isFrame=true&pageId=' + pageId: `${window.location.protocol}//${window.location.host}/mobile.html?isFrame=true&pageId=${pageId}`
+          url: `${process.env.VUE_APP_PAGE_MOBILE}?isFrame=true&pageId=${pageId}`
         },
         {},
         () => {},
