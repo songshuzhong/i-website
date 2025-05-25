e029b37452105390d3398882785e57e86c2f46d6
songshuzhong <18346667355@163.com>
2025-05-25 09:35:34 +0800

    update

--- src/utils/lib.js
+++ src/utils/lib.js
@@ -1,5 +1,5 @@
 import IRenderer from 'i-renderer/dist/js/renderer';
 
-export const loadEditor = () => import('i-renderer/dist/js/renderer');
+export const loadEditor = () => import('i-renderer/dist/js/editor');
 export {IRenderer};
 export {Schema, checkPermission, api} from 'i-renderer/dist/js/renderer';
