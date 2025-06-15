517a561a11ca77a1fe7f55e8b1bf0ae75d4115f0
songshuzhong <18346667355@163.com>
2025-06-15 14:06:10 +0800

    update

--- src/apps/Mobile.vue
+++ src/apps/Mobile.vue
@@ -1,3 +1,62 @@
 <template>
-  <router-view />
+  <div v-if="isFrame" class="i-device-ios">
+    <div class="i-device-inner">
+      <iframe
+        class="i-device-inner__frame"
+        :key="update"
+        :src="`${src}?pageId=${pageId}`"
+      />
+      <div class="i-device-inner__home" @click="refresh"/>
+    </div>
+  </div>
+  <i-schema
+    v-else
+    :init-schema="url"
+    :updatable="false"
+    ref="schema"
+    path="/mobile"
+    classname="i-mobile__container i-website__main i-main__container"
+  />
 </template>
+
+<script>
+import {defineComponent, onMounted, ref} from 'vue';
+import qs from 'qs';
+
+export default defineComponent({
+  name: 'Mobile',
+  setup() {
+    const query = qs.parse(window.location.href.split('?')[1]);
+    const isFrame = ref(query.isFrame);
+    const update = ref(0);
+    let url = '/api/page/' + query.pageId;
+    if (query.shared) {
+      url += '/shared';
+    }
+    url = url.replace('#/', '');
+    const refresh = () => {
+      update.value++;
+    };
+    onMounted(() => {
+      if (!isFrame.value) {
+        const timer = window.setTimeout(() => {
+          const page = document.querySelector('.i-page__container');
+          const doc = document.documentElement;
+          page && page.classList.add('h5');
+          doc.classList.remove('pc');
+          doc.classList.add('mobile');
+          clearTimeout(timer);
+        }, 500);
+      }
+    });
+    return {
+      src: process.env.VUE_APP_PAGE_MOBILE,
+      pageId: query.pageId,
+      isFrame,
+      url,
+      update,
+      refresh
+    };
+  }
+});
+</script>
--- src/apps/Playground.vue
+++ src/apps/Playground.vue
@@ -1,3 +1,59 @@
 <template>
-  <router-view />
+  <div class="i-home__playground">
+    <el-divider>左侧编辑 | 右侧预览</el-divider>
+    <div class="i-home__playground__body">
+      <el-splitter>
+        <el-splitter-panel collapsible>
+          <div v-if="!isEditorReady" v-loading="!isEditorReady" class="i-editor__container" />
+          <component v-else v-bind:is="'i-editor'" :is-json="false"/>
+        </el-splitter-panel>
+        <el-splitter-panel collapsible>
+          <i-schema
+            :init-schema="initSchema"
+            :updatable="false"
+            track="/editor"
+            classname="i-home__playground__preview"
+          />
+        </el-splitter-panel>
+      </el-splitter>
+    </div>
+  </div>
 </template>
+
+<script>
+import {defineComponent, getCurrentInstance, onBeforeMount, onMounted, ref} from 'vue';
+import PLAYGROUND_SCHEMA from '../data/demo';
+import uaManager from '../utils/ua';
+import {loadEditor} from '../utils/lib';
+
+const DEFAULT_SCHEMA = PLAYGROUND_SCHEMA;
+export default defineComponent({
+  name: 'Playground',
+  setup() {
+    const {proxy} = getCurrentInstance();
+    const isEditorReady = ref(false);
+    let notice;
+    onBeforeMount(() => {
+      proxy.$.appContext.$IRenderer = {
+        pageSchema: DEFAULT_SCHEMA
+      };
+      uaManager.setWidth(window.innerHeight);
+      notice = proxy.$message.success('编辑器加载中，请稍等...');
+    });
+    onMounted(() => {
+      loadEditor().then(res => {
+        const {Editor} = res;
+        proxy.$.appContext.components[Editor.name] = Editor;
+        isEditorReady.value = true;
+        notice && notice.close();
+      }).catch(e => {
+        console.error(e);
+      });
+    });
+    return {
+      initSchema: DEFAULT_SCHEMA,
+      isEditorReady
+    };
+  }
+});
+</script>
