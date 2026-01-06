fdab34ff67ab959df25ae924349efeea2749d04e
songshuzhong <songshuzhong@pansoft.com>
2026-01-06 15:23:40 +0800

    update

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,15 @@
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
-  ]
+  ],
+  plugins: [
+    [
+      'import',
+      {
+        libraryName: 'lodash',
+        libraryDirectory: '',
+        camel2DashComponentName: false,
+      },
+    ],
+  ],
 };
