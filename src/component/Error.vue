85125aa9c8edf2b83f03c6bcfb598bb98a829982
songshuzhong <songshuzhong@pansoft.com>
2026-01-07 09:33:07 +0800

    update

--- src/component/Error.vue
+++ src/component/Error.vue
@@ -1,40 +1,60 @@
 <template>
-  <div v-loading="loading" class="i-website__error">
-    <template v-if="!loading">
-      <div class="i-website__error__info">
-        <div class="i-website__error__status">
-          {{status}}
-        </div>
-        <div class="i-website__error__title">
-          {{iError['title']}}
-        </div>
-        <div class="i-website__error__msg">
-          {{iError['message']}}
-        </div>
-        <el-button
+  <el-skeleton
+    :loading="loading"
+    :rows="10"
+    :animated="true"
+  >
+    <template #template>
+      <el-skeleton-item
+          variant="image"
+          style="width: 240px; height: 240px; margin-bottom: 20px;"
+      />
+      <el-skeleton-item variant="h3" style="width: 20%" />
+      <el-skeleton-item variant="h3" style="width: 40%" />
+      <el-skeleton-item variant="h3" style="width: 60%" />
+      <el-skeleton-item variant="h3" style="width: 80%" />
+      <div class="i-schema__container__skeleton">
+        <el-skeleton-item variant="h3" style="margin-right: 16px;" />
+        <el-skeleton-item variant="h3" style="width: 30%;" />
+      </div>
+    </template>
+    <template #default>
+      <div class="i-website__error">
+        <div class="i-website__error__info">
+          <div class="i-website__error__status">
+            {{status}}
+          </div>
+          <div class="i-website__error__title">
+            {{iError['title']}}
+          </div>
+          <div class="i-website__error__msg">
+            {{iError['message']}}
+          </div>
+          <el-button
             type="primary"
             round
             @click="goBack"
             class="i-website__error__action"
-        >
-          返回上一页
-        </el-button>
-        <el-button
+          >
+            返回上一页
+          </el-button>
+          <el-button
             round
             link
             @click="goHome"
             class="i-website__error__action"
-        >
-          返回首页
-        </el-button>
-      </div>
-      <img
+          >
+            返回首页
+          </el-button>
+        </div>
+        <img
           v-if="iError['picture']"
           class="i-website__error__picture"
           :src="iError['picture']"
-      />
+        />
+      </div>
     </template>
-  </div>
+  </el-skeleton>
 </template>
 
 <script>
