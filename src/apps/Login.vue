f8efa2f7ad3febca64609b3137d316055c547053
songshuzhong <songshuzhong@pansoft.com>
2026-01-07 17:17:37 +0800

    update

--- src/apps/Login.vue
+++ src/apps/Login.vue
@@ -14,6 +14,11 @@ export default defineComponent({
   setup() {
     onMounted(() => {
       document.title = '欢迎使用百搭云平台';
+      const color = ['primary', 'success', 'danger'][Math.floor(Math.random() * 3)];
+      const root = document.documentElement.style;
+      root.setProperty('--i-renderer-home-ball-color', `var(--el-color-${color}-light-3)`);
+      root.setProperty('--i-renderer-home-particle-color', `var(--el-color-${color}-light-7)`);
+      root.setProperty('--i-renderer-home-text-color', `var(--el-color-${color})`);
     });
 
     return {
