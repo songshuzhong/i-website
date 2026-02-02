7053c7c85279d014488382a1df89cd2f02cc5875
songshuzhong <songshuzhong@pansoft.com>
2026-02-02 11:09:33 +0800

    update

--- src/utils/lib.js
+++ src/utils/lib.js
@@ -1,12 +1,12 @@
 import IRenderer from 'i-renderer/dist/js/renderer';
 import 'i-renderer/dist/css/index.css';
 
-export const loadEn = () => import('i-renderer/dist/js/locals/en');
-export const loadZh = () => import('i-renderer/dist/js/locals/zh');
-export const loadJa = () => import('i-renderer/dist/js/locals/ja');
-export const loadEditorEn = () => import('i-renderer/dist/js/locals/editor.en');
-export const loadEditorZh = () => import('i-renderer/dist/js/locals/editor.zh');
-export const loadEditorJa = () => import('i-renderer/dist/js/locals/editor.ja');
+export const loadEn = () => import('i-renderer/dist/js/locale/en');
+export const loadZh = () => import('i-renderer/dist/js/locale/zh');
+export const loadJa = () => import('i-renderer/dist/js/locale/ja');
+export const loadEditorEn = () => import('i-renderer/dist/js/locale/editor.en');
+export const loadEditorZh = () => import('i-renderer/dist/js/locale/editor.zh');
+export const loadEditorJa = () => import('i-renderer/dist/js/locale/editor.ja');
 export const loadEditor = () => import('i-renderer/dist/js/editor');
 
 export {IRenderer};
