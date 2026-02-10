faab3e44508ad0873af2b9ddcc0d9e15b53dadca
songshuzhong <songshuzhong@pansoft.com>
2026-02-10 15:27:41 +0800

    update

--- src/utils/lib.js
+++ src/utils/lib.js
@@ -11,3 +11,18 @@ export const loadEditor = () => import('i-renderer/dist/js/editor');
 
 export {IRenderer};
 export {Schema, checkPermission, api} from 'i-renderer/dist/js/renderer';
+
+/*
+import IRenderer from '../../../i-renderer/packages/renderer/index';
+import '../../../i-renderer/packages/assets/styles/index.scss';
+
+export const loadEn = () => import('../../../i-renderer/packages/locale/en');
+export const loadZh = () => import('../../../i-renderer/packages/locale/zh');
+export const loadJa = () => import('../../../i-renderer/packages/locale/ja');
+export const loadEditorEn = () => import('../../../i-renderer/packages/locale/editor.en');
+export const loadEditorZh = () => import('../../../i-renderer/packages/locale/editor.zh');
+export const loadEditorJa = () => import('../../../i-renderer/packages/locale/editor.ja');
+export const loadEditor = () => import('../../../i-renderer/packages/editor');
+
+export {IRenderer};
+export {Schema, checkPermission, api} from '../../../i-renderer/packages/renderer/index';*/
