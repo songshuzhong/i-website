00a73a53136908b15bf22b490f7525e8f81b820a
songshuzhong <songshuzhong@pansoft.com>
2026-02-27 10:27:00 +0800

    update

--- src/data/playground.js
+++ src/data/playground.js
@@ -1,10 +1,13 @@
 export default {
   renderer: 'page',
+  style: {
+    margin: 0
+  },
   body: [
     {
       renderer: 'frame',
       src: process.env.VUE_APP_PAGE_PLAYGROUND
     }
   ],
-  innerStyle: '.i-frame__container {\n  height: calc(100vh - 120px) !important;\n}'
+  innerStyle: '.i-frame__container {\n  height: calc(100vh - 60px) !important;\n}'
 };
