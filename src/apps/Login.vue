8ac2c0870bc794dcf29f138550d04d56a159383e
songshuzhong <18346667355@163.com>
2026-04-20 11:50:17 +0800

    update

--- src/apps/Login.vue
+++ src/apps/Login.vue
@@ -2,16 +2,45 @@
   <i-schema
     :init-schema="loginSchema"
     :updatable="false"
+    :tokens="tokens"
+    @update:lang="updateLang"
   />
 </template>
 
 <script>
-import {defineComponent, onMounted} from 'vue';
+import {defineComponent, getCurrentInstance, onBeforeMount, onMounted, ref} from 'vue';
 import loginSchema from '../data/login.js';
+import {loadEditorEn, loadEditorJa, loadEditorZh, loadEn, loadJa, loadZh} from '@/utils/lib';
 
 export default defineComponent({
   name: 'Login',
   setup() {
+    const { proxy } = getCurrentInstance();
+    const tokens = ref({});
+    const updateLang = (val) => {
+      let promise;
+      if (val === 'zh') {
+        promise = [import('element-plus/es/locale/lang/zh-cn'), loadZh(), loadEditorZh()];
+      } else if (val === 'en') {
+        promise = [import('element-plus/es/locale/lang/en'), loadEn(), loadEditorEn()];
+      } else if (val === 'ja') {
+        promise = [import('element-plus/es/locale/lang/ja'), loadJa(), loadEditorJa()];
+      }
+      Promise.all(promise)
+          .then(([el, renderer, editor]) => {
+            tokens.value = {
+              ...el.default,
+              ...renderer.default,
+              ...editor.default,
+            };
+          })
+          .catch((e) => {
+            console.error(e);
+          });
+    };
+    onBeforeMount(() => {
+      updateLang(proxy.$iRenderConfig.language);
+    });
     onMounted(() => {
       document.title = '欢迎使用百搭云平台';
       const color = ['primary', 'success', 'danger'][Math.floor(Math.random() * 3)];
@@ -22,6 +51,8 @@ export default defineComponent({
     });
 
     return {
+      updateLang,
+      tokens,
       loginSchema
     };
   }
